026f401f3078ba0ac918206abd1eeae636cca943
Blair Wilcox <reccanti@gmail.com>
2024-03-21 17:09:11 -0400

    Start watching directory

--- src/Tokens.ts
+++ src/Tokens.ts
@@ -1,7 +1,9 @@
 import { resolve } from "path";
 import StyleDictionary, { DesignTokens } from "style-dictionary";
 import { mkdirp } from "mkdirp";
-import { writeFile } from "fs/promises";
+import { writeFile, readFile } from "fs/promises";
+import { inspect } from "util";
+import { glob } from "glob";
 
 interface Metadata {
   pluginVersion: string;
@@ -9,9 +11,18 @@ interface Metadata {
 }
 
 // const TEMP_DIR = process.env.TMPDIR || process.env.TMP || process.env.TEMP;
-const TEMP_DIR = resolve(__dirname, "..", "temp");
+const TOKENS_DIR = resolve(__dirname, "..", "tokens");
+const METADATA_DIR = resolve(TOKENS_DIR, "metadata");
+const THEMES_DIR = resolve(TOKENS_DIR, "themes");
 
-console.log(TEMP_DIR);
+const dictionary = StyleDictionary.extend({
+  source: [resolve(TOKENS_DIR, "**/*.json")],
+  platforms: {
+    json: {
+      format: "json",
+    },
+  },
+});
 
 export class Tokens {
   private _metadata: Metadata;
@@ -25,7 +36,56 @@ export class Tokens {
     return this._metadata;
   }
 
-  constructor() {}
+  async initialize() {
+    // Create theme and metadata directories if they don't already exist
+    await Promise.all([mkdirp(METADATA_DIR), mkdirp(THEMES_DIR)]);
+
+    // fetch all theme data and combine it from the themes directory
+    const themeFiles = await glob(resolve(THEMES_DIR, "*.json"));
+
+    let loadedTokens: DesignTokens = {};
+    for (const themeFile of themeFiles) {
+      try {
+        const fileContents = await readFile(themeFile, "utf8");
+        const json = JSON.parse(fileContents);
+        loadedTokens = { ...loadedTokens, ...json };
+      } catch (err) {
+        console.error(`Error processing theme file ${themeFile}, skipping...`);
+      }
+    }
+    this._tokens = loadedTokens;
+
+    // load the metadata
+    const metadataFiles = await glob(resolve(METADATA_DIR, "*.json"));
+    if (!metadataFiles.length) {
+      this._metadata = {
+        pluginVersion: "",
+        updatedAt: new Date().toISOString(),
+      };
+
+      await writeFile(
+        resolve(METADATA_DIR, "metadata.json"),
+        JSON.stringify(this._metadata, null, 2)
+      );
+    } else {
+      let loadedMetadata: Partial<Metadata> = {};
+      for (const metadataFile of metadataFiles) {
+        try {
+          const fileContents = await readFile(metadataFile, "utf8");
+          const json = JSON.parse(fileContents);
+          loadedMetadata = { ...loadedMetadata, ...json };
+        } catch (err) {
+          console.error(
+            `Error processing metadata file ${metadataFile}, skipping...`
+          );
+        }
+      }
+
+      console.log(loadedMetadata);
+
+      this._metadata = loadedMetadata as Metadata;
+    }
+  }
 
   async create(metadata: Metadata) {
     this._tokens = {};
@@ -41,7 +101,7 @@ export class Tokens {
 
   private async writeTempFiles() {
     // create the temp directory if it doesn't exist
-    const tokensDir = resolve(TEMP_DIR, "tokens");
+    const tokensDir = TOKENS_DIR;
     await mkdirp(tokensDir);
 
     // split the token data into separate theme files
@@ -49,16 +109,19 @@ export class Tokens {
 
     const writeFiles = themes.map((theme) =>
       writeFile(
-        resolve(tokensDir, `${theme}.json`),
+        resolve(tokensDir, "themes", `${theme}.json`),
         JSON.stringify(this.tokens[theme], null, 2)
       )
     );
 
     const writeMetadata = writeFile(
-      resolve(tokensDir, "metadata.json"),
+      resolve(tokensDir, "metadata", "metadata.json"),
       JSON.stringify(this.metadata, null, 2)
     );
 
     await Promise.all([...writeFiles, writeMetadata]);
+
+    const out = dictionary.exportPlatform("json");
+    console.log(inspect(out, true, 10, true));
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,81 +2,93 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { inspect } from "util";
+import chokidar from "chokidar";
+import { resolve } from "path";
 
 import { Tokens } from "./Tokens";
 
-const tokens = new Tokens();
+async function start() {
+  const tokens = new Tokens();
+  await tokens.initialize();
 
-const app = express();
-app.use(cors());
-app.use(morgan("combined"));
-app.use(express.json());
+  // watch for changes in the tokens directory
+  // watch(resolve(__dirname, "..", "tokens"), async () => {
+  //   console.log("a change...");
+  // });
+  chokidar.watch(resolve(__dirname, "..", "tokens", "themes")).on("all", () => {
+    console.log("a change...");
+  });
 
-app.post("/figma", async (req, res) => {
-  // if the tokens haven't been created yet, create them and return the
-  // appropriate response
-  if (!tokens.tokens) {
-    console.log("creating...");
-    const pluginVersion = req.body.version;
-    const updatedAt = req.body.updatedAt;
+  const app = express();
+  app.use(cors());
+  app.use(morgan("combined"));
+  app.use(express.json());
 
-    await tokens.create({ pluginVersion, updatedAt });
-    const metadata = tokens.metadata;
+  app.post("/figma", async (req, res) => {
+    // if the tokens haven't been created yet, create them and return the
+    // appropriate response
+    if (!tokens.tokens) {
+      console.log("creating...");
+      const pluginVersion = req.body.version;
+      const updatedAt = req.body.updatedAt;
 
-    return await res.status(201).json({
-      created: true,
-      version: metadata.pluginVersion,
-      updatedAt: metadata.updatedAt,
-    });
-  }
-  // otherwise, return the current values
-  else {
-    console.log("fetching initial...");
+      await tokens.create({ pluginVersion, updatedAt });
+      const metadata = tokens.metadata;
+
+      return await res.status(201).json({
+        created: true,
+        version: metadata.pluginVersion,
+        updatedAt: metadata.updatedAt,
+      });
+    }
+    // otherwise, return the current values
+    else {
+      console.log("fetching initial...");
+
+      const values = tokens.tokens;
+      const metadata = tokens.metadata;
+      return await res.status(200).json({
+        created: false,
+        version: metadata.pluginVersion,
+        updatedAt: metadata.updatedAt,
+        values,
+      });
+    }
+  });
+
+  app.get("/figma", async (req, res) => {
+    console.log("getting...");
 
     const values = tokens.tokens;
     const metadata = tokens.metadata;
+
+    console.log(metadata);
     return await res.status(200).json({
       created: false,
       version: metadata.pluginVersion,
       updatedAt: metadata.updatedAt,
       values,
     });
-  }
-});
-
-app.get("/figma", async (req, res) => {
-  console.log("getting...");
-
-  const values = tokens.tokens;
-  const metadata = tokens.metadata;
-  return await res.status(200).json({
-    created: false,
-    version: metadata.pluginVersion,
-    updatedAt: metadata.updatedAt,
-    values,
   });
-});
 
-app.put("/figma", async (req, res) => {
-  console.log("updating...");
+  app.put("/figma", async (req, res) => {
+    console.log("updating...");
 
-  const pluginVersion = req.body.version;
-  const updatedAt = req.body.updatedAt;
-  const newValues = req.body.values;
+    const pluginVersion = req.body.version;
+    const updatedAt = req.body.updatedAt;
+    const newValues = req.body.values;
 
-  await tokens.update(newValues, { pluginVersion, updatedAt });
-  const metadata = tokens.metadata;
+    await tokens.update(newValues, { pluginVersion, updatedAt });
+    const metadata = tokens.metadata;
 
-  return await res.status(200).json({
-    version: metadata.pluginVersion,
-    updatedAt: metadata.updatedAt,
-    values: tokens.tokens,
+    return await res.status(200).json({
+      version: metadata.pluginVersion,
+      updatedAt: metadata.updatedAt,
+      values: tokens.tokens,
+    });
   });
-});
 
-// app.all("/figma", () => {
-//   console.log("hit!");
-// });
-
-console.log("listening on port 8000");
-app.listen(8000);
+  console.log("listening on port 8000");
+  app.listen(8000);
+}
+start();

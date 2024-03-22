c7419b58ff84658afb8e34f1141b4aa0fc356523
Blair Wilcox <reccanti@gmail.com>
2024-03-22 16:46:09 -0400

    See if this fixes issues

--- src/TokenFileHandler.test.ts
+++ src/TokenFileHandler.test.ts
@@ -1,10 +1,10 @@
 import mock from "mock-fs";
-import { existsSync, accessSync, promises as fsPromises } from "fs";
+// import { existsSync, accessSync, promises as fsPromises } from "fs";
 import { resolve } from "path";
-// import { readFile, readdir } from "fs/promises";
+import { readFile, readdir } from "fs/promises";
 import { DEFAULT_TOKEN_DIR, TokenFileHandler } from "./TokenFileHandler";
 
-const { readdir, readFile, stat, lstat, access } = fsPromises;
+// const { readdir, readFile, stat, lstat, access } = fsPromises;
 
 describe("TokenFileHandler", () => {
   beforeEach(() => {
@@ -19,19 +19,19 @@ describe("TokenFileHandler", () => {
     const handler = new TokenFileHandler();
     await handler.initializeFileStructure();
 
-    const cwd = await readdir(process.cwd(), {
-      recursive: true,
-      withFileTypes: true,
-    });
-    const tokenDir = await readdir(DEFAULT_TOKEN_DIR, {
-      recursive: true,
-      withFileTypes: true,
-    });
+    // const cwd = await readdir(process.cwd(), {
+    //   recursive: true,
+    //   withFileTypes: true,
+    // });
+    // const tokenDir = await readdir(DEFAULT_TOKEN_DIR, {
+    //   recursive: true,
+    //   withFileTypes: true,
+    // });
 
     // console.log(cwd);
     // console.log(tokenDir);
-    const s = accessSync(process.cwd());
-    console.log(s);
+    // const s = accessSync(process.cwd());
+    // console.log(s);
 
     // console.log(mock.getMockRoot());
     // console.log(DEFAULT_TOKEN_DIR);
@@ -41,6 +41,14 @@ describe("TokenFileHandler", () => {
     // expect(existsSync(DEFAULT_TOKEN_DIR)).toBe(true);
     // expect(existsSync(handler.themesDir)).toBe(true);
     // expect(existsSync(handler.metadataDir)).toBe(true);
+
+    // expect(await readdir(DEFAULT_TOKEN_DIR)).not.toThrow();
+    // expect(await readdir(handler.themesDir)).not.toThrow();
+    // expect(await readdir(handler.metadataDir)).not.toThrow();
+
+    await expect(readdir(DEFAULT_TOKEN_DIR)).resolves;
+    await expect(readdir(handler.themesDir)).resolves;
+    await expect(readdir(handler.metadataDir)).resolves;
 
     // expect the metadata file to be initialized correctly
     const metadataStr = await readFile(
@@ -58,10 +66,15 @@ describe("TokenFileHandler", () => {
     await handler.initializeFileStructure();
 
     // expect the file structure to be initialized correctly
-    expect(existsSync(DEFAULT_TOKEN_DIR)).toBe(false);
-    expect(existsSync(testDir)).toBe(true);
-    expect(existsSync(handler.themesDir)).toBe(true);
-    expect(existsSync(handler.metadataDir)).toBe(true);
+    // expect(await readdir(DEFAULT_TOKEN_DIR)).toThrow();
+    // expect(existsSync(testDir)).toBe(true);
+    // expect(existsSync(handler.themesDir)).toBe(true);
+    // expect(existsSync(handler.metadataDir)).toBe(true);
+
+    await expect(readdir(DEFAULT_TOKEN_DIR)).rejects.toThrow();
+    await expect(readdir(testDir)).resolves;
+    await expect(readdir(handler.themesDir)).resolves;
+    await expect(readdir(handler.metadataDir)).resolves;
 
     // expect the metadata file to be initialized correctly
     const metadataStr = await readFile(
